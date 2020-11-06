3880d9e6a6eb548a6e7bba1efdd2d2acca3c3161
krystalmm <taeyeon_aina@live.jp>
2020-11-06 13:28:17 +0900

    add validation name and email should not be too long

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,4 +18,30 @@ RSpec.describe User, type: :model do
     expect(user_a).not_to be_valid
   end
 
+  it "name should not be too long" do
+    user_a.name = "a" *51
+    expect(user_a).not_to be_valid
+  end
+
+  it "email should not be too long" do
+    user_a.email = "a" *244 + "@example.com"
+    expect(user_a).not_to be_valid
+  end
+ 
+  it "email validation should accept valid adresses" do
+    valid_addresses = %w[ user@example.com USER@foo.COM A_US-ER@foo.bar.org first.last@foo.jp alice+bob@baz.cn ]
+    valid_addresses.each do | valid_address |
+      user_a.email = valid_address
+      expect(user_a).to be_valid, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  it "email validation should reject invalid addresses" do
+    invalid_addresses = %w[ user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com ]
+    invalid_addresses.each do | invalid_address |
+      user_a.email = invalid_address
+      expect(user_a).to_not be_valid, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+
 end
