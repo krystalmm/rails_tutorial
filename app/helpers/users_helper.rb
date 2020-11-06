801a7c32c4dc88cf3754224377d7a328441f174b
krystalmm <taeyeon_aina@live.jp>
2020-11-06 22:01:56 +0900

    add gravatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  # 引数で与えられたユーザーのGravatar画像を返す
+  # GravatarのURLはユーザーのメアドをＭＤ５という仕組みでハッシュ化しているので、Digestライブラリのhexdigestメソッドを使うとMD5のハッシュ化が実現できる！
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
