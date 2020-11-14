5d43a9a68df2662ff62e45b03328b2e88edb9bf6
krystalmm <taeyeon_aina@live.jp>
2020-11-14 20:36:23 +0900

    Implement advanced login

--- spec/requests/access_to_sessions_spec.rb
+++ spec/requests/access_to_sessions_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'access to sessions', type: :request do
+RSpec.describe 'access to sessions', type: :request do
   let!(:user) { FactoryBot.create(:user) }
   describe 'POST#create' do
     it 'log in and redirect to detail page' do
@@ -22,6 +22,19 @@ describe 'access to sessions', type: :request do
       delete logout_path
       expect(response).to redirect_to root_url
       expect(is_logged_in?).to be_falsey
+    end
+  end
+
+  describe 'remember me' do
+    it "remembers the cookie when user checks the remember me box" do
+      log_in_as(user)
+      # expect(cookies[:remember_token]).to_not eq nil
+      expect(cookies[:remember_token]).to eq assigns(:user).remember_token
+    end
+
+    it "does not remembers the cookie when user does not checks the remember me box" do
+      log_in_as(user, remember_me: '0')
+      expect(cookies[:remember_token]).to eq nil
     end
   end
 end
