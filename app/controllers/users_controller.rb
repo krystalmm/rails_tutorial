6395530d822b38c868c4b1d0c16f1ca234413c7e
krystalmm <taeyeon_aina@live.jp>
2020-11-16 21:04:02 +0900

    add edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,11 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit 
+    @user = User.find(params[:id])
+  end
+
+
   
   private
 
--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,11 +1,14 @@
 require 'rails_helper'
 
-describe "access to users" do
-  describe 'GET#new' do
-    it 'returns http success' do
-      visit signup_path
-      expect(page).to have_css('h1', text: 'Sign up')
-      expect(page).to have_title "Sign up | Ruby on Rails Tutorial Sample App"
+RSpec.describe type: :system do
+  describe "access to users" do
+    describe 'GET#new' do
+      it 'returns http success' do
+        visit signup_path
+        expect(page).to have_css('h1', text: 'Sign up')
+        expect(page).to have_title "Sign up | Ruby on Rails Tutorial Sample App"
+      end
     end
   end
 end
+
