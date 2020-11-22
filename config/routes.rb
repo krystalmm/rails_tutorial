a29f387050151ae162a23a2538ccf7e05fd619ea
krystalmm <taeyeon_aina@live.jp>
2020-11-22 10:20:10 +0900

    add update action to  password_resets_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   root 'static_pages#home'
 
 
@@ -22,6 +26,8 @@ Rails.application.routes.draw do
   resources :users
 
   resources :account_activations, only:[:edit]
+
+  resources :password_resets, only:[:new, :create, :edit, :update]
          
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,8 +1,10 @@
 require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
+
+  let(:user) { FactoryBot.create(:user) }
+
   describe "account_activation" do
-    let(:user) { FactoryBot.create(:user) }
     let(:mail) { UserMailer.account_activation(user) }
 
     it "renders the headers" do
@@ -18,18 +20,22 @@ RSpec.describe UserMailer, type: :mailer do
     end
   end
 
-  # describe "password_reset" do
-  #   let(:mail) { UserMailer.password_reset }
+  describe "password_reset" do
 
-  #   it "renders the headers" do
-  #     expect(mail.subject).to eq("Password reset")
-  #     expect(mail.to).to eq(["to@example.org"])
-  #     expect(mail.from).to eq(["from@example.com"])
-  #   end
+    before { user.reset_token = User.new_token }
 
-  #   it "renders the body" do
-  #     expect(mail.body.encoded).to match("Hi")
-  #   end
-  # end
+    let(:mail) { UserMailer.password_reset(user) }
+
+    it "renders the headers" do
+      expect(mail.subject).to eq("Password reset")
+      expect(mail.to).to eq([user.email])
+      expect(mail.from).to eq(["noreply@example.com"])
+    end
+
+    it "renders the body" do
+      expect(mail.body.encoded).to match user.reset_token
+      expect(mail.body.encoded).to match CGI.escape(user.email)
+    end
+  end
 
 end
